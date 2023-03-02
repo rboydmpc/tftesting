c49c24ed6f27b270ad4d10f3597eacfd9b2dee05
rboydmpc <73500396+rboydmpc@users.noreply.github.com>
2023-03-02 18:24:20 -0500

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -11,3 +11,51 @@ provider "aws" {
     access_key = var.access_key
     secret_key = var.secret_key
   }
+
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+   default = "us-east-1"
+}
+
+variable "bucket_prefix" {
+   description = "Enter mutiliple bucket prefix names to cretae multiple buckets like ''test'' or ''test'', ''new''"
+}
+
+variable "acl_value" {
+   default = "private"
+}
+
+variable "bucket_count" {
+}
+
+variable "force_destroy" {
+    type = bool   
+    default = "false"
+}
+
+variable "mytest" {
+}
+resource "aws_s3_bucket" "s3" {
+    count = var.bucket_count
+    bucket_prefix = var.bucket_prefix
+    force_destroy = var.force_destroy
+    tags = {
+    Date = timestamp()
+    mytest = var.mytest
+    }
+}
+output "arn" {
+  description = "ARN of the bucket"
+  value       = aws_s3_bucket.s3[0].arn
+}
+
+output "name" {
+  description = "Name (id) of the bucket"
+  value       = aws_s3_bucket.s3[0].id
+}
+
+output "domainname" {
+  description = "Domain NAme of Bucket"
+  value       = aws_s3_bucket.s3[0].bucket_domain_name
+}
