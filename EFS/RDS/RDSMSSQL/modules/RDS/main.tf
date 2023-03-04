620e1be8e968fdf07165d210b760f2ab88e77d1f
rboydmpc <rboyd@morpheusdata.com>
2023-03-04 16:25:58 -0500

    Update main.tf

--- EFS/RDS/RDSMSSQL/modules/RDS/main.tf
+++ EFS/RDS/RDSMSSQL/modules/RDS/main.tf
@@ -4,12 +4,4 @@ locals {
   var_iops = {
     value = var.storage_type == "io1" ? max(var.iops, var.max_allocated_storage*0.5) : null
   }
-  var_storage = {
-    value = max(var.allocated_storage, (var.max_allocated_storage*0.02))
-  }
-  var_az_subnets = {
-    value = compact([local.vpc_subnet_az1, local.vpc_subnet_az2])
-  }
-  time = "${timestamp()}"
-  
 }
